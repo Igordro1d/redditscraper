b2c694b27617a7e8dd9d510bf312087ff148fa36
Igordro1d <igormoreiracastro0@gmail.com>
2025-01-05 07:00:02 +1300

    checks submission titles to see if they contain a stock symbol

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import praw
 from dotenv import load_dotenv
 import os
 import logging
+import re
+import yfinance as yf
 
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
@@ -18,3 +20,21 @@ reddit = praw.Reddit(
     user_agent=os.getenv("REDDIT_USER_AGENT"),
     username=os.getenv("REDDIT_USERNAME")
 )
+
+def is_stock_symbol(symbol):
+    try:
+        stock = yf.Ticker(symbol)
+        print(f"Checking symbol: {symbol}")
+        print(stock.info)
+        if stock.info.get("regularMarketPrice") is not None:
+            return True
+    except Exception:
+        pass
+    return False
+
+real_symbols = []
+for submission in reddit.subreddit("RVSN").hot(limit=25):
+    print(submission.title)
+    potential_symbol = re.findall(r'\b[A-Z]{2,5}\b', submission.title)
+    real_symbols.extend([symbol for symbol in potential_symbol if is_stock_symbol(symbol)])
+print(real_symbols)
