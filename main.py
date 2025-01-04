292a299f915d49d33ece09ca15c4f3dcab6f5470
Igordro1d <igormoreiracastro0@gmail.com>
2025-01-04 15:38:15 +1300

    created access to reddit account

---
+++ main.py
@@ -0,0 +1,20 @@
+import praw
+from dotenv import load_dotenv
+import os
+import logging
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+for logger_name in ("praw", "prawcore"):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+load_dotenv()
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    password=os.getenv("REDDIT_PASSWORD"),
+    user_agent=os.getenv("REDDIT_USER_AGENT"),
+    username=os.getenv("REDDIT_USERNAME")
+)
